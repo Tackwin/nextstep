251a67c44194783065439bf5da5c54946171cec9
Luca Bourroux <lucabrx@gmail.com>
2025-01-07 01:04:16 +0100

    Initial commit.

---
+++ Build.cpp
@@ -0,0 +1,25 @@
+#include "Ease.hpp"
+
+EASE_WATCH_ME;
+
+Build build(Flags flags) noexcept {
+	flags.no_install_path = true;
+	
+	Build b = Build::get_default(flags);
+	b.flags.no_default_lib = true;
+	b.flags.entry_point = "start";
+	b.flags.subsystem = Flags::Subsystem::Console;
+	b.flags.disable_exceptions = true;
+
+	b.name = "nextstep";
+
+	b.add_header("src/");
+	b.add_source_recursively("src/");
+	b.add_define("WIN32_LEAN_AND_MEAN");
+	b.add_define("NOMINMAX");
+
+	b.add_library("kernel32.lib");
+
+
+	return b;
+}
